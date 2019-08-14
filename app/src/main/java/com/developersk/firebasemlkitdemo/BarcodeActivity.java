0d8b10906cb53c68b5d5db3edf49e77d14c1d524
shivamkumard107 <shivam.kumard107@gmail.com>
2019-08-14 15:14:51 +0530

    Added Barcode Reader Feature

--- app/src/main/java/com/developersk/firebasemlkitdemo/BarcodeActivity.java
+++ app/src/main/java/com/developersk/firebasemlkitdemo/BarcodeActivity.java
@@ -1,14 +1,95 @@
 package com.developersk.firebasemlkitdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class BarcodeActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.util.List;
+
+public class BarcodeActivity extends BaseActivity {
+    private ImageView mImageView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
+
+        mImageView = findViewById(R.id.image_view);
+        mTextView = findViewById(R.id.text_view);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap;
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RC_STORAGE_PERMS1:
+                case RC_STORAGE_PERMS2:
+                    checkStoragePermission(requestCode);
+                    break;
+                case RC_SELECT_PICTURE:
+                    Uri dataUri = data.getData();
+                    String path = MyHelper.getPath(this, dataUri);
+                    if (path == null) {
+                        bitmap = MyHelper.resizeImage(imageFile, this, dataUri, mImageView);
+                    } else {
+                        bitmap = MyHelper.resizeImage(imageFile, path, mImageView);
+                    }
+                    if (bitmap != null) {
+                        mTextView.setText(null);
+                        mImageView.setImageBitmap(bitmap);
+                        barcodeDetector(bitmap);
+                    }
+                    break;
+                case RC_TAKE_PICTURE:
+                    bitmap = MyHelper.resizeImage(imageFile, imageFile.getPath(), mImageView);
+                    if (bitmap != null) {
+                        mTextView.setText(null);
+                        mImageView.setImageBitmap(bitmap);
+                        barcodeDetector(bitmap);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void barcodeDetector(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        //FirebaseVisionBarcodeDetectorOptions.Builder builder = new FirebaseVisionBarcodeDetectorOptions.Builder();
+        //FirebaseVisionBarcodeDetectorOptions options = builder.setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE).build();
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
+        detector.detectInImage(image).addOnSuccessListener(firebaseVisionBarcodes -> {
+            mTextView.setText(getInfoFromBarcode(firebaseVisionBarcodes));
+            Log.e("Barcode Activity", "detected");
+        }).addOnFailureListener(e -> {
+            mTextView.setText(R.string.error_detect);
+            Log.e("Barcode Activity", "nothing detected");
+        });
+    }
+
+    private String getInfoFromBarcode(List<FirebaseVisionBarcode> barcodes) {
+        StringBuilder result = new StringBuilder();
+        for (FirebaseVisionBarcode barcode : barcodes) {
+            //int valueType = barcode.getValueType();
+            result.append(barcode.getRawValue() + "\n");
+        }
+        if ("".equals(result.toString())) {
+            return getString(R.string.error_detect);
+        } else {
+            return result.toString();
+        }
     }
 }
