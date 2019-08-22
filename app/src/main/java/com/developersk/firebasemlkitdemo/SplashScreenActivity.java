a8811fc34a2920834defe589d5fe48fc3dafd7cb
shivamkumard107 <shivam.kumard107@gmail.com>
2019-08-22 19:32:18 +0530

    added splash screen of firebase

---
+++ app/src/main/java/com/developersk/firebasemlkitdemo/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.developersk.firebasemlkitdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_splash_screen);
+        // Using handler with postDelayed called runnable run method
+        new Handler().postDelayed(() -> {
+
+            Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+
+            startActivity(i);
+
+            // close this activity
+
+            finish();
+
+        }, 3000); // wait for 3 seconds
+    }
+}
